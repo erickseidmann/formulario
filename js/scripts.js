a752180b657208847acee289f1815f00457b6f35
FlaviaEvelyn44 <evelynflavia44@gmail.com>
2022-06-24 12:08:08 -0300

    evolucao do formulario

--- js/scripts.js
+++ js/scripts.js
@@ -9,25 +9,7 @@ function pessoa(tipo){
 }
 
 
-function mascara(mascara, input) {
-    const vetmasc = mascara.split("")
-    const num = input.value.replace(/\D/g, "")
-    const cursor = input.selectionStart
-    
-    for(let i=0; i<num.lengt; i++) {
-        vetmasc.splice(vetmasc.indexOf("#"), 1, num[i])
-    }
 
-    input.value = vetmasc.join("")
-
-    if (tecla != 37 && (cursor == 3 || cursor == 7 || cursor == 11)) {
-        input.selectionRanger(cursor ++, cursor++)
-    }else{
-        input.setSelectioRange(cursor, cursor)
-    }
-    
-
-}
 
 
 
